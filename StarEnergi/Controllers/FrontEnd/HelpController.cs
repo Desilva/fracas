dad88d55d788fde4104b180f370495187c21cd3c
Yosef <yosefsukianto@gmail.com>
2013-09-16 15:15:09 +0700

    Add help content

--- StarEnergi/Controllers/FrontEnd/HelpController.cs
+++ StarEnergi/Controllers/FrontEnd/HelpController.cs
@@ -29,41 +29,47 @@ namespace StarEnergi.Controllers.FrontEnd
         {
             string file = "";
             if (type == "Fracas_Event_Log")
+                file = Server.MapPath(Url.Content("~/Content/Help/FRACAS Event Log.pdf"));
+            else if (type == "Historical_Data")
+                file = Server.MapPath(Url.Content("~/Content/Help/Historical Data.pdf"));
+            else if (type == "Incident_Report")
                 file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Historical_Data")
+            else if (type == "Incident_Investigation_Report")
                 file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Incident_Report")
+            else if (type == "Injury_Report")
                 file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Incident_Investigation_Report")
+            else if(type == "Audit")
                 file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Injury_Report")
+            else if (type == "Observation_Form")
                 file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if(type = "Audit")
+            else if (type == "Daily_Log")
+                file = Server.MapPath(Url.Content("~/Content/Help/Daily Log.pdf"));
+            else if (type == "Equipt._Report")
+                file = Server.MapPath(Url.Content("~/Content/Help/Equipment Daily Report.pdf"));
+            else if (type == "Asset_Register")
+                file = Server.MapPath(Url.Content("~/Content/Help/Asset Register.pdf"));
+            else if (type == "RCA")
                 file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Observation_Form")
-                file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Daily_Log")
-                file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Equipt._Report")
-                file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "Asset_Register")
-                file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "RCA")
-                file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            else if (type = "PIR")
-                file = Server.MapPath(Url.Content("~/Content/Help/road-map.pdf"));
-            PdfReader reader = new PdfReader(file);
-            MemoryStream pdfStream = new MemoryStream();
+            else if (type == "PIR")
+                file = Server.MapPath(Url.Content("~/Content/Help/PIR.pdf"));
+            if (file != "")
+            {
+                PdfReader reader = new PdfReader(file);
+                MemoryStream pdfStream = new MemoryStream();
 
-            PdfStamper pdfStamper = new PdfStamper(reader, pdfStream);
+                PdfStamper pdfStamper = new PdfStamper(reader, pdfStream);
 
-            reader.Close();
-            pdfStamper.Close();
-            pdfStream.Flush();
-            pdfStream.Close();
+                reader.Close();
+                pdfStamper.Close();
+                pdfStream.Flush();
+                pdfStream.Close();
 
-            byte[] pdfArray = pdfStream.ToArray();
-            return new BinaryContentResult(pdfArray, "application/pdf");
+                byte[] pdfArray = pdfStream.ToArray();
+                return new BinaryContentResult(pdfArray, "application/pdf");
+            }
+            else {
+                return null;
+            }
         }
     }
 }
