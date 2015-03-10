e061a688f1e855005d9874e284b8f2e494eb53a1
Yosef Sukianto <yosefsukianto@licht-soft.com>
2015-03-10 17:07:47 +0700

    for pull

--- StarEnergi/Models/Relmon.Designer.cs
+++ StarEnergi/Models/Relmon.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'D:\kerjaan\Project\4Sync\My Documents\PROJECT\ONGOING\Star Energi Geo\SOLUTION\fracas\StarEnergi\Models\Relmon.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\yosef\Documents\PROJECT\Star Energi Geo\SOLUTION\FRACAS\StarEnergi\Models\Relmon.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
